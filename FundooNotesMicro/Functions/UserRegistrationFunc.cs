887955cdd37272ab132ce322034eb6c50f44a70c
Shubham Seth <shubhamsth1234@gmail.com>
2022-04-14 15:35:42 +0530

    [Shubham] added User Microservice

---
+++ FundooNotesMicro/Functions/UserRegistrationFunc.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using UserService.Models;
+
+namespace UserService.Functions
+{
+    public class UserRegistrationFunc
+    {
+        private readonly IConfiguration _config;
+        private readonly CosmosClient _cosmosClient;
+        
+        public UserRegistrationFunc(CosmosClient cosmosClient, IConfiguration config)
+        {
+            _cosmosClient = cosmosClient;
+            _config = config;
+        }
+
+        [FunctionName("UserRegistration")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Creating a new User Details item.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var data = JsonConvert.DeserializeObject<UserRegModel>(requestBody);
+
+            var user = new UserRegModel()
+            {
+                Id = data.Id,
+                FirstName = data.FirstName,
+                LastName = data.LastName,
+                Email = data.Email,
+                Password = data.Password,
+                ConfirmPassword = data.ConfirmPassword,
+                CreatedAt = DateTime.Now
+            };
+
+            var container = this._cosmosClient.GetContainer("UserDB", "UserDetails");
+
+            try
+            {
+                var result = await container.CreateItemAsync(user, new PartitionKey(user.Id));
+                return new OkObjectResult(result.Resource);
+            }
+            catch (CosmosException cosmosException)
+            {
+                log.LogError("Creating item failed with error {0}", cosmosException.ToString());
+                return new BadRequestObjectResult($"Failed to create item. Cosmos Status Code {cosmosException.StatusCode}, Sub Status Code {cosmosException.SubStatusCode}: {cosmosException.Message}.");
+            }
+        }
+    }
+}
---
+++ FundooNotesMicro/Models/UserRegModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Models
+{
+    public class UserRegModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
