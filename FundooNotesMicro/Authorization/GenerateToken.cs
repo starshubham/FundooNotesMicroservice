a82e14870bf3d2919c307abec25880138c9234f7
Shubham Seth <shubhamsth1234@gmail.com>
2022-04-15 13:00:17 +0530

    [Shubham] added User login and forget password functions

---
+++ FundooNotesMicro/Authorization/GenerateToken.cs
@@ -0,0 +1,40 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Authorization
+{
+    public class GenerateToken
+    {
+        private readonly IJwtAlgorithm _algorithm;
+        private readonly IJsonSerializer _serializer;
+        private readonly IBase64UrlEncoder _base64Encoder;
+        private readonly IJwtEncoder _jwtEncoder;
+        public GenerateToken()
+        {
+            // JWT specific initialization.
+            _algorithm = new HMACSHA256Algorithm();
+            _serializer = new JsonNetSerializer();
+            _base64Encoder = new JwtBase64UrlEncoder();
+            _jwtEncoder = new JwtEncoder(_algorithm, _serializer, _base64Encoder);
+        }
+
+        public string IssuingToken(String Id)
+        {
+            Dictionary<string, object> claims = new Dictionary<string, object>
+            {
+                // JSON representation of the user Reference with ID and display name
+                {
+                    "UserRegModel",
+                    Id
+                }
+            };
+            string token = _jwtEncoder.Encode(claims, "ldbudnsvjdnvbsvhsiksncbcvsgwhkaamcbvbdguhrjfkfnmdnbsvwhkmdbbdhjdm dbbdjdkmdnbbjjkdmsnsbdbh");
+            return token;
+
+        }
+    }
+}
---
+++ FundooNotesMicro/Functions/ForgetPasswordFunc.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using UserService.Authorization;
+using Microsoft.Azure.Documents.Client;
+using UserService.Models;
+using System.Linq;
+
+namespace UserService.Functions
+{
+    public class ForgetPasswordFunc
+    {
+        GenerateToken auth = new GenerateToken();
+
+        [FunctionName("ForgetPasswordFunc")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                ConnectionStringSetting = "CosmosDBConnection"
+                )]
+                DocumentClient client,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<ForgetPassword>(requestBody);
+
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("UserDB", "UserDetails");
+            var document = client.CreateDocumentQuery<UserRegModel>(collectionUri).Where(t => t.Email == data.Email)
+                    .AsEnumerable().FirstOrDefault();
+            if (document != null)
+            {
+                var token = this.auth.IssuingToken(document.Id.ToString());
+                return new OkObjectResult(token);
+
+            }
+            return (ActionResult)new NotFoundResult();
+        }
+    }
+}
